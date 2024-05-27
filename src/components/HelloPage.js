e25a395b97df55d86ff694ca0a04cf506d429e2d
yahya elmokhtari <128697438+YAHYA280@users.noreply.github.com>
2024-05-27 09:38:54 +0100

    front end changes

--- src/components/HelloPage.js
+++ src/components/HelloPage.js
@@ -3,7 +3,7 @@ import React from "react";
 export const HelloPage = () => {
   return (
     <div>
-      <div>This peoject was made by : Yahya Elmokhtari & Souhail Elmahdani</div>
+      <div>This project was made by : Yahya Elmokhtari & Souhail Elmahdani</div>
       <div>for more details please visit our repositorie in github : </div>
       <div>
         <a href="https://github.com/YAHYA280/deyployment-ReactApp">
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,14 +5,13 @@ export const HomePage = () => {
     <div>
       <h1>Hello This Project made for studying purposes</h1>
       <h1>
-        If you use an adress ip to access this page means the deploiment of this
-        project was succesfull
+        If you use an IP address to access this page, it means the deployment of
+        this project was successful.
       </h1>
       <div>
-        <img
-          alt="monkey"
-          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExanhhdmQ1bmVpNG01bmpmMHFpeHNtYW9sc2w3dXJiem9hcHMwcWJkZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5Zesu5VPNGJlm/giphy.gif"
-        ></img>
+        <span>
+          ...............................................................;
+        </span>
       </div>
     </div>
   );
